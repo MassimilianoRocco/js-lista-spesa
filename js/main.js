e9e1fcf12b1c2d82c08582e2c3b1beb8dbcb8f59
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-04-24 16:13:14 +0200

    Added: M3 - stamp in "li" element

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
 const list = ["Cyberpunk2077", "Fallout 4", "Star Citizen", "Total War Warhammer 3", "Rainbow Six Siege"];
 console.log(list);
 
+//CICLO FOR
+const ulListA = document.querySelector("#list_A");
+
 let element;
 for(let x=0; x<list.length;x++){
     element = list[x];
     console.log(element);
+    
+    // const liElement = document.createElement("li");
+	// liElement.innerText = element;
+	// ulList.append(liElement);
+    ulListA.innerHTML += `<li>${element}</li>`;  
 }
+
 element = null;
+
+//Separatore console
 console.log("______________________________________");
+
+//CICLO WHILE
+const ulListB = document.querySelector("#list_B");
 
 let contatore = 0;
 while(contatore<list.length){
     element = list[contatore];
     console.log(element);
 
+    ulListB.innerHTML += `<li>${element}</li>`;
     contatore++;
 }
+
+
