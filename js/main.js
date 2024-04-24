7f8d2026b33d52d24b198e40daae11e43138a87f
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-04-24 15:29:33 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const list = ["Cyberpunk2077", "Fallout 4", "Star Citizen", "Total War Warhammer 3", "Rainbow Six Siege"];
+console.log(list);
+
+let element;
+for(let x=0; x<list.length;x++){
+    element = list[x];
+    console.log(element);
+}
